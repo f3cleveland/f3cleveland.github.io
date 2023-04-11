f4bfeb02796387980f8466dea581936bcfcc1e95
f3cleveland <f3cleveland@gmail.com>
2023-04-11 17:09:27 -0400

    Initial commit

---
+++ nojson.js
@@ -0,0 +1,17 @@
+var nojson = [
+"<div class='grid-item '><img width='80px' height='15px' src='badges/birthmonth/aquarius1.gif' alt='aq1'/><span class='tooltiptext'><p class='filename'><em>Name: </em>birthmonth</p><p class='category'><em>Category: </em>aquarius1</p><p class='filetype'><em>Type: </em>.gif</p></span></div>"
+,
+"<div class='grid-item '><img width='80px' height='15px' src='badges/birthmonth/scorpio2.gif' alt='scorpio2'/><span class='tooltiptext'><p class='filename'><em>Name: </em>birthmonth</p><p class='category'><em>Category: </em>scorpio2</p><p class='filetype'><em>Type: </em>.gif</p></span></div>"
+,
+"<div class='grid-item Anchor bg ft f '><a href=badges/badges.html><img width='80px' height='15px' src='badges/batoplist.gif' alt='SEPT'/><span class='tooltiptext'><p class='filename'><em>Name: </em>birthmonth</p><p class='category'><em>Category: </em>september</p><p class='filetype'><em>Type: </em>.gif</p></span></div>"
+,
+"<div class='grid-item Anchor'><img width='80px' height='15px' src='badges/birthmonth/taurus1.gif' alt='taurus1'/><span class='tooltiptext'><p class='filename'><em>Name: </em>birthmonth</p><p class='category'><em>Category: </em>taurus1</p><p class='filetype'><em>Type: </em>.gif</p></span></div>"
+,
+"<div class='grid-item Anchor ft'><img width='80px' height='15px' src='badges/birthmonth/taurus2.gif' alt='taurus2'/><span class='tooltiptext'><p class='filename'><em>Name: </em>birthmonth</p><p class='category'><em>Category: </em>taurus2</p><p class='filetype'><em>Type: </em>.gif</p></span></div>"
+,
+"<div class='grid-item '><img width='80px' height='15px' src='badges/birthmonth/virgo1.gif' alt='virgo1'/><span class='tooltiptext'><p class='filename'><em>Name: </em>birthmonth</p><p class='category'><em>Category: </em>virgo1</p><p class='filetype'><em>Type: </em>.gif</p></span></div>"
+,
+"<div class='grid-item '><img width='80px' height='15px' src='badges/birthmonth/virgo2.gif' alt='virgo2'/><span class='tooltiptext'><p class='filename'><em>Name: </em>birthmonth</p><p class='category'><em>Category: </em>virgo2</p><p class='filetype'><em>Type: </em>.gif</p></span></div>"
+,
+"<div class='grid-item preloader tooltip blogs gif still'><img width='80px' height='15px' src='badges/blogs/1month21days.gif' alt='1mnt21'/><span class='tooltiptext'><p class='filename'><em>Name: </em>blogs</p><p class='category'><em>Category: </em>1month21days</p><p class='filetype'><em>Type: </em>.gif</p></span></div>"
+];
